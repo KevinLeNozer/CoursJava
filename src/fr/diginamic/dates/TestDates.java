de795ca6c50c0ccd4a47cb2a7a87cb8e5a7d81fc
KevinLeNozer <kevin.lenozer@gmail.com>
2022-06-10 10:00:24 +0200

    modifCalendar

--- src/fr/diginamic/dates/TestDates.java
+++ src/fr/diginamic/dates/TestDates.java
@@ -5,9 +5,12 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class TestDates {
+    public static final String DATE_FORMAT_TP1 = "dd/MM/yyyy";
+    public static final String DATE_FORMAT_TP2 = "yyyy/MM/dd HH:mm:ss";
+
     public static void main(String[] args) throws ParseException {
 
-        SimpleDateFormat formateur = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat formateur = new SimpleDateFormat(DATE_FORMAT_TP1);
 
         Date date1 = new Date();
         String chaine1 = formateur.format(date1);
@@ -15,8 +18,8 @@ public class TestDates {
 
         System.out.println(chaine1);
 
-        SimpleDateFormat formateur2 = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-        Date date3 = new Date("19/05/2016 23:59:30");
+        SimpleDateFormat formateur2 = new SimpleDateFormat(DATE_FORMAT_TP2);
+        Date date3 = new Date("19-05/2016 23:59:30");
         String chaine3 = formateur2.format(date3);
 
         System.out.println(chaine3);
